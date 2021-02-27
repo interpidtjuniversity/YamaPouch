042d4b034f16d7f379a3dd63c906c201a2c30b69
interpidtjuniversity <cy120571672@gmail.com>
2021-02-27 19:01:36 +0800

    fix bug

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 	"os"
 )
 
-const usage = `mydocker is a simple container runtime implementation.
+const usage = `yamaPouch is a simple container runtime implementation.
 			   The purpose of this project is to learn how docker works and how to write a docker by ourselves
 			   Enjoy it, just for fun.`
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "mydocker"
+	app.Name = "yamaPouch"
 	app.Usage = usage
 
 	app.Commands = []cli.Command{
